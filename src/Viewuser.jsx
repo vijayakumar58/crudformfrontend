56606ecb96c04a14953e0ab731cd2c99f41b69c9
vijayakumar58 <vijayalingam58@gmail.com>
2023-12-19 13:42:01 +0530

    add

--- src/Viewuser.jsx
+++ src/Viewuser.jsx
@@ -62,7 +62,7 @@ const Viewuser = () => {
       </div>
       <div className="col-3 ">
           {/* <img src={userData.image ? require(`./images/${userData.image}`) : null} alt="A beautiful picture of a landscape" style={{height:"250px", width:"340px",padding:""}} /> */}
-          <img src={userData.image ? `${env.api}/images/${userData.image}` : null} alt="User Avatar" style={{ height: "250px", width: "340px", padding: "" }} />
+          <img src={userData.image ? `${env.api}/src/images/${userData.image}` : null} alt="User Avatar" style={{ height: "250px", width: "340px", padding: "" }} />
         </div>
         </div>
       </div>

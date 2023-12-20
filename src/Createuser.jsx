65b4eec7ecdbaa206f5ec1024e7ded4a4f9092d4
vijayakumar58 <vijayalingam58@gmail.com>
2023-12-20 07:26:03 +0530

    add

--- src/Createuser.jsx
+++ src/Createuser.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./createuser.css";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
@@ -7,6 +7,7 @@ import { env } from "./config";
 
 const Createuser = () => {
   const navigate = useNavigate();
+  const [isLoading, setLoading] = useState();
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -40,126 +41,139 @@ const Createuser = () => {
     },
     onSubmit: async (values) => {
       const formData = new FormData();
-      formData.append('name',values.name)
-      formData.append('email',values.email)
-      formData.append('degree',values.degree)
-      formData.append('homeaddress',values.homeaddress)
-      formData.append('mobile',values.mobile)
-      formData.append('image',values.image)
+      formData.append("name", values.name);
+      formData.append("email", values.email);
+      formData.append("degree", values.degree);
+      formData.append("homeaddress", values.homeaddress);
+      formData.append("mobile", values.mobile);
+      formData.append("image", values.image);
 
-      let createuser = await axios.post(
-        `${env.api}/createUser`,formData, {
+      setLoading(true);
+      let createuser = await axios.post(`${env.api}/createUser`, formData, {
         headers: {
-          'authorization' : window.localStorage.getItem("app-token"),
-          'Content-Type': 'multipart/form-data'
+          authorization: window.localStorage.getItem("app-token"),
+          "Content-Type": "multipart/form-data",
         },
       });
       alert("User created successfully");
       navigate("/mainPage");
+      setLoading(false);
     },
   });
   return (
     <div id="createuser">
       <div id="formContainer">
-        <form className="user" id="form" onSubmit={formik.handleSubmit}>
-          <fieldset>
-            <h1>Registration Form</h1>
-            <div className="form-group row">
-              <div className="col-sm-6 mb-3 mb-sm-0">
-                <label style={{ color: "black" }}>Name</label>
-                <input
-                  type="text"
-                  className="form-control form-control-user"
-                  id={`${formik.errors.name ? `input-error` : ``}`}
-                  placeholder="Name"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  name="name"
-                />
-                <span style={{ color: "red" }}>{formik.errors.name}</span>
+        {isLoading ? (
+          <button class="btn btn-primary" type="button" disabled>
+            <span
+              class="spinner-border spinner-border-sm"
+              aria-hidden="true"
+            ></span>
+            <span role="status">Loading...</span>
+          </button>
+        ) : (
+          <form className="user" id="form" onSubmit={formik.handleSubmit}>
+            <fieldset>
+              <h1>Registration Form</h1>
+              <div className="form-group row">
+                <div className="col-sm-6 mb-3 mb-sm-0">
+                  <label style={{ color: "black" }}>Name</label>
+                  <input
+                    type="text"
+                    className="form-control form-control-user"
+                    id={`${formik.errors.name ? `input-error` : ``}`}
+                    placeholder="Name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    name="name"
+                  />
+                  <span style={{ color: "red" }}>{formik.errors.name}</span>
+                </div>
+                <div className="col-sm-6">
+                  <lable style={{ color: "black" }}>Email</lable>
+                  <input
+                    type="text"
+                    className="form-control form-control-user"
+                    id={`${formik.errors.email ? `input-error` : ``}`}
+                    placeholder="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    name="email"
+                  />
+                  <span style={{ color: "red" }}>{formik.errors.email}</span>
+                </div>
               </div>
-              <div className="col-sm-6">
-                <lable style={{ color: "black" }}>Email</lable>
-                <input
-                  type="text"
-                  className="form-control form-control-user"
-                  id={`${formik.errors.email ? `input-error` : ``}`}
-                  placeholder="Email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  name="email"
-                />
-                <span style={{ color: "red" }}>{formik.errors.email}</span>
+              <div className="form-group row">
+                <div className="col-sm-6 mb-3 mb-sm-0">
+                  <lable style={{ color: "black" }}>Degree</lable>
+                  <input
+                    type="text"
+                    className="form-control form-control-user"
+                    id={`${formik.errors.degree ? `input-error` : ``}`}
+                    placeholder="degree"
+                    value={formik.values.degree}
+                    onChange={formik.handleChange}
+                    name="degree"
+                  />
+                  <span style={{ color: "red" }}>{formik.errors.degree}</span>
+                </div>
+                <div className="col-sm-6">
+                  <lable style={{ color: "black" }}>Home Address</lable>
+                  <input
+                    type="text"
+                    className="form-control form-control-user"
+                    id={`${formik.errors.homeaddress ? `input-error` : ``}`}
+                    placeholder="Home Address"
+                    value={formik.values.homeaddress}
+                    onChange={formik.handleChange}
+                    name="homeaddress"
+                  />
+                  <span style={{ color: "red" }}>
+                    {formik.errors.homeaddress}
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-sm-6 mb-3 mb-sm-0">
-                <lable style={{ color: "black" }}>Degree</lable>
-                <input
-                  type="text"
-                  className="form-control form-control-user"
-                  id={`${formik.errors.degree ? `input-error` : ``}`}
-                  placeholder="degree"
-                  value={formik.values.degree}
-                  onChange={formik.handleChange}
-                  name="degree"
-                />
-                <span style={{ color: "red" }}>{formik.errors.degree}</span>
+              <div className="form-group row">
+                <div className="col-sm-6 mb-3 mb-sm-0">
+                  <lable style={{ color: "black" }}>Mobile Numbder</lable>
+                  <input
+                    type="number"
+                    className="form-control form-control-user"
+                    id={`${formik.errors.mobile ? `input-error` : ``}`}
+                    placeholder="Mobile Number"
+                    value={formik.values.mobile}
+                    onChange={formik.handleChange}
+                    name="mobile"
+                  />
+                  <span style={{ color: "red" }}>{formik.errors.mobile}</span>
+                </div>
+                <div className="col-sm-6">
+                  <lable style={{ color: "black" }}>Image</lable>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    className="form-control form-control-user"
+                    id={`${formik.errors.image ? `input-error` : ``}`}
+                    placeholder="Image"
+                    // value={formik.values.image}
+                    onChange={(e) =>
+                      formik.setFieldValue("image", e.target.files[0])
+                    }
+                    name="image"
+                  />
+                  <span style={{ color: "red" }}>{formik.errors.image}</span>
+                </div>
               </div>
-              <div className="col-sm-6">
-                <lable style={{ color: "black" }}>Home Address</lable>
-                <input
-                  type="text"
-                  className="form-control form-control-user"
-                  id={`${formik.errors.homeaddress ? `input-error` : ``}`}
-                  placeholder="Home Address"
-                  value={formik.values.homeaddress}
-                  onChange={formik.handleChange}
-                  name="homeaddress"
-                />
-                <span style={{ color: "red" }}>
-                  {formik.errors.homeaddress}
-                </span>
-              </div>
-            </div>
-            <div className="form-group row">
-              <div className="col-sm-6 mb-3 mb-sm-0">
-                <lable style={{ color: "black" }}>Mobile Numbder</lable>
-                <input
-                  type="number"
-                  className="form-control form-control-user"
-                  id={`${formik.errors.mobile ? `input-error` : ``}`}
-                  placeholder="Mobile Number"
-                  value={formik.values.mobile}
-                  onChange={formik.handleChange}
-                  name="mobile"
-                />
-                <span style={{ color: "red" }}>{formik.errors.mobile}</span>
-              </div>
-              <div className="col-sm-6">
-                <lable style={{ color: "black" }}>Image</lable>
-                <input
-                  type="file" 
-                  accept='image/*'
-                  className="form-control form-control-user"
-                  id={`${formik.errors.image ? `input-error` : ``}`}
-                  placeholder="Image"
-                  // value={formik.values.image}
-                  onChange={(e) => formik.setFieldValue("image", e.target.files[0])}
-                  name="image"
-                />
-                <span style={{ color: "red" }}>{formik.errors.image}</span>
-              </div>
-            </div>
-            <input
-              className="btn btn-user btn-block"
-              id="submit"
-              type={"submit"}
-              value="Register Account"
-              disabled={!formik.isValid}
-            />
-          </fieldset>
-        </form>
+              <input
+                className="btn btn-user btn-block"
+                id="submit"
+                type={"submit"}
+                value="Register Account"
+                disabled={!formik.isValid}
+              />
+            </fieldset>
+          </form>
+        )}
       </div>
     </div>
   );
--- src/Loginpage.jsx
+++ src/Loginpage.jsx
@@ -1,95 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import "./loginpage.css";
 import { useFormik } from "formik";
 import axios from "axios";
-import {env} from './config'
+import { env } from "./config";
 import { Link, useNavigate } from "react-router-dom";
 
 const Loginpage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
   const formik = useFormik({
-    initialValues:{
-      email : "",
-      password : ""
+    initialValues: {
+      email: "",
+      password: "",
     },
-    validate : (values) => {
-      let errors = {}
+    validate: (values) => {
+      let errors = {};
       if (values.email === "") {
-        errors.email = "Please enter a valid email"
-      } 
-      if (values.password === "") {
-        errors.password = "Please enter a valid password"
+        errors.email = "Please enter a valid email";
       }
-      return errors
+      if (values.password === "") {
+        errors.password = "Please enter a valid password";
+      }
+      return errors;
     },
-    onSubmit : async (values) => {
+    onSubmit: async (values) => {
       try {
-        const loginData = await axios.post(`${env.api}/login`,values);
+        setLoading(true);
+        const loginData = await axios.post(`${env.api}/login`, values);
         if (loginData.status === 200) {
-          navigate("/mainPage")
-          window.localStorage.setItem("app-token",loginData.data)
+          navigate("/mainPage");
+          window.localStorage.setItem("app-token", loginData.data);
+          setLoading(false);
         } else {
-          
         }
       } catch (error) {
         alert(error.response.data.message);
       }
-    }
-  })
+    },
+  });
   return (
     <div className="loginpage">
-    <div className="loginbody">
-    <div className="form form-login">
-      <form onSubmit={formik.handleSubmit}>
-        <div className="form-header">
-          <img
-            className="logo"
-            src="https://media.licdn.com/dms/image/C4D0BAQH746NGxDEPsQ/company-logo_200_200/0/1631340704531?e=2147483647&v=beta&t=Wx_y-mCmh_KgZph4nOldeYP4EYPGSgig-sPZs9NnmNs"
-            alt="Reon Logo"
-          />
-          <h3 className="text-white">Reon Technologies</h3>
+      <div className="loginbody">
+        <div className="form form-login">
+          {isLoading ? (
+            <button class="btn btn-primary" type="button" disabled>
+              <span
+                class="spinner-border spinner-border-sm"
+                aria-hidden="true"
+              ></span>
+              <span role="status">Loading...</span>
+            </button>
+          ) : (
+            <form onSubmit={formik.handleSubmit}>
+              <div className="form-header">
+                <img
+                  className="logo"
+                  src="https://media.licdn.com/dms/image/C4D0BAQH746NGxDEPsQ/company-logo_200_200/0/1631340704531?e=2147483647&v=beta&t=Wx_y-mCmh_KgZph4nOldeYP4EYPGSgig-sPZs9NnmNs"
+                  alt="Reon Logo"
+                />
+                <h3 className="text-white">Reon Technologies</h3>
+              </div>
+              <div className="form-body">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="input-slideInRight"
+                    id={`${formik.errors.email ? `input-error` : ``}`}
+                    placeholder="E-mail"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    name="email"
+                  />
+                  <span style={{ color: "red" }}>{formik.errors.email}</span>
+                  <label for="email" className="label-slideInRight">
+                    E-mail
+                  </label>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="input-slideInRight"
+                    id={`${formik.errors.password ? `input-error` : ``}`}
+                    placeholder="Password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    name="password"
+                  />
+                  <label for="senha" className="label-slideInRight">
+                    Password
+                  </label>
+                  <span style={{ color: "red" }}>{formik.errors.password}</span>
+                </div>
+                <div className="spacing">
+                  or continue with{" "}
+                  <Link className="highlight" to="/createLoginAccount">
+                    Create Account
+                  </Link>
+                </div>
+              </div>
+              <div className="form-footer">
+                <input type="submit" value="Login" />
+              </div>
+            </form>
+          )}
         </div>
-        <div className="form-body">
-          <div className="form-group">
-            <input
-              type="email"
-              className="input-slideInRight"
-              id={`${formik.errors.email ? `input-error`: ``}`}
-              placeholder="E-mail"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              name="email"
-            />
-            <span style={{color:"red"}}>{formik.errors.email}</span>
-            <label for="email" className="label-slideInRight">
-              E-mail
-            </label>
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="input-slideInRight"
-              id={`${formik.errors.password ? `input-error`:``}`}
-              placeholder="Password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              name="password"
-            />
-            <label for="senha" className="label-slideInRight">
-              Password
-            </label>
-            <span style={{color:"red"}}>{formik.errors.password}</span>
-          </div>
-          <div className="spacing">
-              or continue with <Link className="highlight" to="/createLoginAccount">Create Account</Link>
-            </div>
-        </div>
-        <div className="form-footer">
-          <input type="submit" value="Login" />
-        </div>
-      </form>
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
